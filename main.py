62178c6cc03c78b70255063cdaf941b13f2907fa
Kacper Stasica <kacperstasica@yahoo.pl>
2022-03-21 18:16:39 +0100

    Change root redirect url

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from starlette import status
 from starlette.responses import RedirectResponse
 
@@ -13,6 +16,8 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(cars.router)
 app.include_router(reviews.router)
+# stop redirecting paths without trailing slash to slash (we want response 404 instead of 307)
+app.router.redirect_slashes = False
 
 
 def get_db():
@@ -23,6 +28,14 @@ def get_db():
         db.close()
 
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+
 @app.get("/health")
 async def health():
     return {"status": status.HTTP_200_OK}
@@ -30,7 +43,7 @@ async def health():
 
 @app.get("/")
 async def root():
-    return RedirectResponse(url="/cars", status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url="/docs", status_code=status.HTTP_302_FOUND)
 
 
 if __name__ == "__main__":
