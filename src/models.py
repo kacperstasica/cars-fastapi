f4d10ec91a1b6248dd46f03038c5772c260d2337
Kacper Stasica <kacperstasica@yahoo.pl>
2022-03-22 09:33:04 +0100

    Cleanup

--- src/models.py
+++ src/models.py
@@ -5,21 +5,21 @@ from src.database import Base
 
 
 class Car(Base):
-    __tablename__ = "cars"
-
     id = Column(Integer, primary_key=True, index=True)
     make = Column(String)
     model = Column(String, unique=True)
+
+    __tablename__ = "cars"
 
     reviews = relationship("Review", back_populates="car")
 
 
 class Review(Base):
-    __tablename__ = "reviews"
-
     id = Column(Integer, primary_key=True, index=True)
     rating = Column(Integer)
     car_id = Column(Integer, ForeignKey("cars.id", ondelete="cascade"), nullable=True)
+
+    __tablename__ = "reviews"
 
     car = relationship(
         "Car",
