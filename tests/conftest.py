dfce0a09bbefc84099a7d5c900ccbce1c562a4d6
Kacper Stasica <kacperstasica@yahoo.pl>
2022-03-23 08:48:43 +0100

    Comment out factory-boy for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
+from unittest.mock import patch
+
+# import factory
 import pytest
 from fastapi.testclient import TestClient
 
 from main import app
+
 import sys
 import os
 
@@ -12,3 +16,20 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 def client():
     with TestClient(app) as test_client:
         yield test_client
+
+#
+# @pytest.fixture()
+# def mocked_response():
+#     with patch(
+#             "services.car_existence_checker.CarExistenceChecker.get_cars_response",
+#             return_value=[
+#                 {
+#                     'Make_ID': 482,
+#                     'Make_Name': 'Foo',
+#                     'Model_ID': 1951,
+#                     'Model_Name': 'Bar'
+#                 }
+#             ]
+#     ):
+#         result = CarExistenceChecker("Foo", "Bar")
+#         yield result
--- tests/test_api/test_health.py
+++ tests/test_api/test_health.py
@@ -1,11 +1,54 @@
-from tests.factories import CarFactory
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from starlette import status
+
+from main import app
+from src.database import Base
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+#
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+# TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#
+#
+# Base.metadata.create_all(bind=engine)
+
+
+# def override_get_db():
+#     try:
+#         db = TestingSessionLocal()
+#         yield db
+#     finally:
+#         db.rollback()
+#         db.execute(CLEAR_DB_SQL)
+#         db.commit()
+#         db.close()
+
+
+# app.dependency_overrides[get_db] = override_get_db
 
 
 def test_health(client):
     response = client.get("/health")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["status"] == status.HTTP_200_OK
 
 
-def test_delete_car(client):
-    car = CarFactory(model="Golf", make="Volkswagen")
-    # to be continued
+# def test_create_car(client, mocked_response):
+#     response = client.post(
+#         "/cars/",
+#         json={"make": "Foo", "model": "Bar"}
+#     )
+#
+#     assert response.status_code == status.HTTP_201_CREATED
+#     data = response.json()
+#     assert data["model"] == "Bar"
+#     assert "id" in data
+#     car_id = data["id"]
+#
+#     response = client.get(f"/{car_id}/")
+#     assert response.status_code == status.HTTP_200_OK
+#     data = response.json()
+#     assert data["model"] == "Bar"
